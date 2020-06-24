475b7991e338340c15713182ae590fcaff438044
GaneshkumarKvt <ganeshkumarmuruga@gmail.com>
2020-06-24 21:49:28 +0530

    parse server login activity ready

---
+++ app/src/main/java/com/ganeshkumar/parseemailverification/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.ganeshkumar.parseemailverification;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText edtLoginUsername,edtloginpassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        edtLoginUsername = findViewById(R.id.edt_login);
+        edtloginpassword = findViewById(R.id.edtloginpass);
+    }
+
+    public void Loginispressed(View btnview){
+
+        ParseUser.logInInBackground(edtLoginUsername.getText().toString(), edtloginpassword.getText().toString(), new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (user != null){
+                    if (user.getBoolean("emailVerified")){
+                        alertDisplayer("Login Sucessful", "Welcome, " + edtLoginUsername.getText().toString()+"!",false);
+                    }else
+                    {
+                        ParseUser.logOut();
+                        alertDisplayer("Login Fail", "Please Verify Your Email First",true);
+                    }
+                }else{
+                    ParseUser.logOut();
+                    alertDisplayer("Login Fail", e.getMessage() + " Please re-try",true);
+                }
+            }
+        });
+
+
+    }
+    private void alertDisplayer(String title,String message, final boolean error){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        if(!error){
+                            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        }
+                    }
+                });
+        AlertDialog ok = builder.create();
+        ok.show();
+
+    }
+}
--- app/src/main/java/com/ganeshkumar/parseemailverification/MainActivity.java
+++ app/src/main/java/com/ganeshkumar/parseemailverification/MainActivity.java
@@ -1,14 +1,23 @@
 package com.ganeshkumar.parseemailverification;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText Email, username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +25,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ParseInstallation.getCurrentInstallation().saveInBackground();
-
-
+        Email = findViewById(R.id.edtEmail);
+        username = findViewById(R.id.edtUsername);
+        password = findViewById(R.id.edtPassword);
 
     }
 
     public void SignUptapped(View btnView){
 
         Toast.makeText(this, "Sign Up is pressed", Toast.LENGTH_SHORT).show();
+
+        try {
+            ParseUser user = new ParseUser();
+            user.setUsername(username.getText().toString());
+            user.setPassword(password.getText().toString());
+            user.setEmail(Email.getText().toString());
+
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e == null){
+                        ParseUser.logOut();
+                        alertDisplayer("Account Created Successfully!", "Please verify your email before Login", false);
+                    } else {
+                        ParseUser.logOut();
+                        alertDisplayer("Error Account Creation failed", "Account could not be created"+" :"+e.getMessage(), true);
+                    }
+                }
+            });
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private void alertDisplayer(String title,String message, final boolean error){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        if(!error){
+                            Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        }
+                    }
+                });
+        AlertDialog ok = builder.create();
+        ok.show();
 
     }
 
