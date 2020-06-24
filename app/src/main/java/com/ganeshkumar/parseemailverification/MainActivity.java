7f20ccc32d5b521f92cfa19fa3759b5032ec118e
GaneshkumarKvt <ganeshkumarmuruga@gmail.com>
2020-06-24 19:28:19 +0530

    Layout created

--- app/src/main/java/com/ganeshkumar/parseemailverification/MainActivity.java
+++ app/src/main/java/com/ganeshkumar/parseemailverification/MainActivity.java
@@ -1,8 +1,12 @@
 package com.ganeshkumar.parseemailverification;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.ParseInstallation;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
+
+
     }
+
+    public void SignUptapped(View btnView){
+
+        Toast.makeText(this, "Sign Up is pressed", Toast.LENGTH_SHORT).show();
+
+    }
+
 }
